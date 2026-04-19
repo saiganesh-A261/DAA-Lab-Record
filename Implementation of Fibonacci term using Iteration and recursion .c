4e0b5f68cdceb77279e8250a8e0df36a76c470e6
Sai Ganesh Pabolu <11249a261@kanchiuniv.ac.in>
2026-04-19 12:49:27 +0530

    Create Implementation of Fibonacci term using Iteration and recursion .c

---
+++ Implementation of Fibonacci term using Iteration and recursion .c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int fib(int n)
+{
+if(n==0){
+return 0;
+}
+if(n==1){
+return 1;
+{
+fib(n-1)+fib(n-2);
+return fib(n-1)+fib(n-2)
+}
+int main()
+{
+int n,a=0,b=1,c;
+printf("enter n value:");
+scanf("%d",&n);
+if(n<0){
+printf("invalidd entry/n")
+}
+printf("fibonacci term is:%d",fib(n));
+return 0;
+}
+
+// Code By @saiganesh-A261
